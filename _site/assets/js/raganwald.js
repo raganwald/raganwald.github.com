473257db874889255e718e3d1270ec6ab401bb1e
Reginald Braithwaite <reg@braythwayt.com>
2013-03-18 12:32:54 -0400

    show code

---
+++ _site/assets/js/raganwald.js
@@ -0,0 +1,24 @@
+jQuery(function () {
+  
+  var hiddenCodeEls = $('.hide-code pre');
+  
+  jQuery.each(hiddenCodeEls, function (index, element) {
+    var hider = $('<p></p>')
+      .append('(')
+      .append(
+        $('<a href="#">show code</a>')
+          .click(function () {
+            $(element).show();
+            hider.hide();
+            return false;
+          })
+      )
+      .append(')');
+    
+    $(element)
+      .before(hider)
+      .hide();
+      
+  });
+  
+});
--- _site/specs.command
+++
@@ -1,7 +0,0 @@
-#!/bin/sh
-
-cd ~/Dropbox/sites/raganwald.github.com/_posts/enchanted-forest
-coffee --output ~/Dropbox/sites/raganwald.github.com/_specs --compile *.coffee.md
-jasmine-node ~/Dropbox/sites/raganwald.github.com/_specs --matchall
-
-
