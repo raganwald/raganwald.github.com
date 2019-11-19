6c136696266dc68ee3ef0ed22cc097621b7588e1
Reginald Braithwaite <reg@braythwayt.com>
2019-11-19 08:43:06 -0500

    Update 10-nary.js

--- assets/supplemental/fsa/10-nary.js
+++ assets/supplemental/fsa/10-nary.js
@@ -5,39 +5,18 @@ function union (a, ...args) {
 
   const [b, ...rest] = args;
 
-  const {
-    states: aDeclaredStates,
-    accepting: aAccepting
-  } = validatedAndProcessed(a);
-  const aStates = [''].concat(aDeclaredStates);
-  const {
-    states: bDeclaredStates,
-    accepting: bAccepting
-  } = validatedAndProcessed(b);
-  const bStates = [''].concat(bDeclaredStates);
-
-  const statesAAccepts =
-    aAccepting.flatMap(
-      aAcceptingState => bStates.map(bState => abToAB(aAcceptingState, bState))
-    );
-  const statesBAccepts =
-    bAccepting.flatMap(
-      bAcceptingState => aStates.map(aState => abToAB(aState, bAcceptingState))
-    );
-  const allAcceptingStates =
-    statesAAccepts.concat(
-      statesBAccepts.filter(
-        state => statesAAccepts.indexOf(state) === -1
+  const ab =
+    deDup(
+      reachableFromStart(
+        powerset(
+          removeEpsilonTransitions(
+            epsilonUnion(a, b)
+          )
+        )
       )
     );
 
-  const productAB = product(a, b);
-  const { stateSet: reachableStates } = validatedAndProcessed(productAB);
-
-  const { start, transitions } = productAB;
-  const accepting = allAcceptingStates.filter(state => reachableStates.has(state));
-
-  return union({ start, accepting, transitions }, ...rest);
+  return union(ab, ...rest);
 }
 
 function intersection (a, ...args) {
@@ -65,7 +44,9 @@ function intersection (a, ...args) {
   const { start, transitions } = productAB;
   const accepting = allAcceptingStates.filter(state => reachableStates.has(state));
 
-  return intersection({ start, accepting, transitions }, ...rest);
+  const ab = deDup({ start, accepting, transitions });
+
+  return intersection(ab, ...rest);
 }
 
 function catenation (a, ...args) {
@@ -75,13 +56,16 @@ function catenation (a, ...args) {
 
   const [b, ...rest] = args;
 
-  const ab = powerset(
-    reachableFromStart(
-      removeEpsilonTransitions(
-        epsilonCatenate(a, b)
+  const ab =
+    deDup(
+      powerset(
+        reachableFromStart(
+          removeEpsilonTransitions(
+            epsilonCatenate(a, b)
+          )
+        )
       )
-    )
-  );
+    );
 
   return catenation(ab, ...rest);
 }
