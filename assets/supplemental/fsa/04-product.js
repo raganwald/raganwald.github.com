da2d40f8dfc29a4fe639b26b315834f6bae1c8dd
Reg Braithwaite <reg@braythwayt.com>
2019-12-02 18:21:17 -0500

    one more step

--- assets/supplemental/fsa/04-product.js
+++ assets/supplemental/fsa/04-product.js
@@ -1,4 +1,4 @@
-console.log('04-product.js')
+console.log('04-product-and-union.js')
 
 // This function computes a state name for the product given
 // the names of teh states for a and b
@@ -110,3 +110,85 @@ function product (a, b, mode = 'union') {
   return { start, accepting, transitions };
 
 }
+
+// 04-union-and-intersection.js
+
+function union2 (a, b) {
+  const {
+    states: aDeclaredStates,
+    accepting: aAccepting
+  } = validatedAndProcessed(a);
+  const aStates = [''].concat(aDeclaredStates);
+  const {
+    states: bDeclaredStates,
+    accepting: bAccepting
+  } = validatedAndProcessed(b);
+  const bStates = [''].concat(bDeclaredStates);
+
+  const statesAAccepts =
+    aAccepting.flatMap(
+      aAcceptingState => bStates.map(bState => abToAB(aAcceptingState, bState))
+    );
+  const statesBAccepts =
+    bAccepting.flatMap(
+      bAcceptingState => aStates.map(aState => abToAB(aState, bAcceptingState))
+    );
+  const allAcceptingStates =
+    statesAAccepts.concat(
+      statesBAccepts.filter(
+        state => statesAAccepts.indexOf(state) === -1
+      )
+    );
+
+  const productAB = product(a, b);
+  const { stateSet: reachableStates } = validatedAndProcessed(productAB);
+
+  const { start, transitions } = productAB;
+  const accepting = allAcceptingStates.filter(state => reachableStates.has(state));
+
+  return { start, accepting, transitions };
+}
+
+function intersection2 (a, b) {
+  const {
+    accepting: aAccepting
+  } = validatedAndProcessed(a);
+  const {
+    accepting: bAccepting
+  } = validatedAndProcessed(b);
+
+  const allAcceptingStates =
+    aAccepting.flatMap(
+      aAcceptingState => bAccepting.map(bAcceptingState => abToAB(aAcceptingState, bAcceptingState))
+    );
+
+  const productAB = product(a, b);
+  const { stateSet: reachableStates } = validatedAndProcessed(productAB);
+
+  const { start, transitions } = productAB;
+  const accepting = allAcceptingStates.filter(state => reachableStates.has(state));
+
+  return { start, accepting, transitions };
+}
+
+// ----------
+
+verify(union2(reg, uppercase), {
+  '': true,
+  'r': false,
+  'R': true,
+  'Reg': true,
+  'REG': true,
+  'Reginald': false,
+  'REGINALD': true
+});
+
+verify(intersection2(reg, uppercase), {
+  '': false,
+  'r': false,
+  'R': false,
+  'Reg': false,
+  'REG': true,
+  'Reginald': false,
+  'REGINALD': false
+});
